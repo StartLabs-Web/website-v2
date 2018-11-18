1c07c2cabfe277614261d34e4c1c16018c84b884
Leon Cheng <57leonardo@gmail.com>
2018-11-18 18:47:39 -0500

    moved old website into new folder, now have two links: old and new

--- Main/main.py
+++ Main/main.py
@@ -2,9 +2,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Index Page
-@app.route('/')
-def index():
+@app.route('/new')
+def new_version():
     return render_template('index.html')
+
+# Old version of Startlabs Website
+@app.route('/')
+def old_version():
+    return render_template('old/indexold.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
