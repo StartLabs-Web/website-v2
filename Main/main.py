8841c81479c78bbd47f8efbcb8e5388c8541d925
Jeremy McCulloch <jeremymcculloch@Dell-Laptop-8.local>
2019-02-27 21:48:25 -0500

    Added / fixed style of footer

--- Main/main.py
+++ Main/main.py
@@ -10,6 +10,20 @@ app = Flask(__name__)
 num_future_events = 5
 num_past_events = 3
 
+# For debugging only
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                 endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
 # Index Page
 @app.route('/new')
 def new_version():
