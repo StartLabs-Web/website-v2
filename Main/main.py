1d5ae9f13fb1c3399bd0455a1fddbe99e35c102e
Leon Cheng <57leonardo@gmail.com>
2019-04-11 16:55:42 -0400

    renamed index to home.html, removed home from navbar links

--- Main/main.py
+++ Main/main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 # Redesign
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
 @app.route('/about')
 def about():
