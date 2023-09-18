b2dff121228b10604ec5a48845c9fbdb897802f7
Isaiah <isaiahnat10@gmail.com>
2023-09-18 15:46:13 -0400

    New techtrek and events page

--- Main/main.py
+++ Main/main.py
@@ -69,7 +69,11 @@ def add_to_mailing_list():
 @app.route('/events')
 def events():
     # return render_template("construction.html", page="events", colors=colors)
-    return render_template("large_events.html", page="events", colors=colors)
+    return render_template("events.html", page="events", colors=colors)
+
+@app.route('/techtrek')
+def techtrek():
+    return render_template("techtrek.html", page="techtrek", colors=colors)
 
 @app.route('/partners')
 def partners():
